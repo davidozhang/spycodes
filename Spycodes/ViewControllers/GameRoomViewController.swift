2a424ffcae5691a6127e868f9ad98a85d1d6ecac
David Zhang <davzee@hotmail.com>
2016-07-23 02:07:26 -0700

    Refactor Game Room View Controller

--- Spycodes/ViewControllers/GameRoomViewController.swift
+++ Spycodes/ViewControllers/GameRoomViewController.swift
@@ -5,14 +5,7 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     private let reuseIdentifier = "game-room-view-cell"
     private let edgeInset: CGFloat = 12
     private let playerDisconnectedString = "A player from your team has disconnected."
-    
-    var multipeerManager = MultipeerManager.instance
-    var audioToolboxManager = AudioToolboxManager.instance
-    
-    var player = Player.instance
-    var cardCollection = CardCollection.instance
-    var round = Round.instance
-    var room = Room.instance
+
     var connectedPeers: [MCPeerID: String]?
     
     private var broadcastTimer: NSTimer?
@@ -41,15 +34,15 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.multipeerManager.delegate = self
+        MultipeerManager.instance.delegate = self
         
-        self.round.setStartingTeam(self.cardCollection.getStartingTeam())
+        Round.instance.setStartingTeam(CardCollection.instance.getStartingTeam())
         
-        if self.player.isHost() {
+        if Player.instance.isHost() {
             self.broadcastTimer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(GameRoomViewController.broadcastData), userInfo: nil, repeats: true)  // Broadcast host's card collection every 2 seconds
         }
         
-        if self.player.isClueGiver() {
+        if Player.instance.isClueGiver() {
             self.endRoundButton.hidden = false
         } else {
             self.endRoundButton.hidden = true
@@ -57,7 +50,7 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
         
         self.refreshTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(GameRoomViewController.refreshView), userInfo: nil, repeats: true)    // Refresh room every second
         
-        self.teamLabel.text = self.player.getTeam() == Team.Red ? "Red" : "Blue"
+        self.teamLabel.text = Player.instance.getTeam() == Team.Red ? "Red" : "Blue"
         self.confirmButton.hidden = true
     }
     
@@ -78,29 +71,29 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     
     @objc
     private func broadcastData() {
-        var data = NSKeyedArchiver.archivedDataWithRootObject(self.cardCollection)
-        self.multipeerManager.broadcastData(data)
+        var data = NSKeyedArchiver.archivedDataWithRootObject(CardCollection.instance)
+        MultipeerManager.instance.broadcastData(data)
         
-        data = NSKeyedArchiver.archivedDataWithRootObject(self.round)
-        self.multipeerManager.broadcastData(data)
+        data = NSKeyedArchiver.archivedDataWithRootObject(Round.instance)
+        MultipeerManager.instance.broadcastData(data)
     }
     
     private func updateDashboard() {
-        self.cardsRemainingLabel.text = String(self.cardCollection.getCardsRemainingForTeam(self.player.getTeam()))
+        self.cardsRemainingLabel.text = String(CardCollection.instance.getCardsRemainingForTeam(Player.instance.getTeam()))
         
-        if self.round.currentTeam == self.player.getTeam() {
+        if Round.instance.currentTeam == Player.instance.getTeam() {
             if self.clueTextField.isFirstResponder() || self.numberOfWordsTextField.isFirstResponder() {
                 return  // Cluegiver is editing the clue/number of words
             }
             
-            if self.round.isClueSet() {
-                self.clueTextField.text = self.round.clue
+            if Round.instance.isClueSet() {
+                self.clueTextField.text = Round.instance.clue
             }
             else {
-                if self.player.isClueGiver() {
+                if Player.instance.isClueGiver() {
                     self.clueTextField.text = Round.defaultClueGiverClue
                     self.confirmButton.hidden = false
-                    if self.round.isClueSet() && self.round.isNumberOfWordsSet() {
+                    if Round.instance.isClueSet() && Round.instance.isNumberOfWordsSet() {
                         self.clueTextField.enabled = false
                         self.numberOfWordsTextField.enabled = false
                     } else {
@@ -113,8 +106,8 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
                 }
             }
             
-            if self.round.isNumberOfWordsSet() {
-                self.numberOfWordsTextField.text = self.round.numberOfWords
+            if Round.instance.isNumberOfWordsSet() {
+                self.numberOfWordsTextField.text = Round.instance.numberOfWords
             }
             else {
                 self.numberOfWordsTextField.text = Round.defaultNumberOfWords
@@ -126,11 +119,11 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     }
     
     private func updateEndRoundButton() {
-        if !self.player.isClueGiver() {
+        if !Player.instance.isClueGiver() {
             return
         }
         
-        if self.round.currentTeam == self.player.getTeam() {
+        if Round.instance.currentTeam == Player.instance.getTeam() {
             self.endRoundButton.alpha = 1.0
             self.endRoundButton.enabled = true
         }
@@ -141,7 +134,7 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     }
     
     private func updateConfirmButton() {
-        if !self.player.isClueGiver() || self.round.currentTeam != self.player.getTeam() {
+        if !Player.instance.isClueGiver() || Round.instance.currentTeam != Player.instance.getTeam() {
             return
         }
         
@@ -155,8 +148,8 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     }
     
     private func didConfirm() {
-        self.round.clue = self.clueTextField.text
-        self.round.numberOfWords = self.numberOfWordsTextField.text
+        Round.instance.clue = self.clueTextField.text
+        Round.instance.numberOfWords = self.numberOfWordsTextField.text
         self.clueTextField.enabled = false
         self.numberOfWordsTextField.enabled = false
         self.confirmButton.hidden = true
@@ -164,7 +157,7 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     }
     
     private func didEndRound() {
-        self.round.endRound(self.player.getTeam())
+        Round.instance.endRound(Player.instance.getTeam())
         self.broadcastData()
     }
     
@@ -175,13 +168,13 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     
     func didReceiveData(data: NSData, fromPeer peerID: MCPeerID) {
         if let cardCollection = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? CardCollection {
-            self.cardCollection = cardCollection
+            CardCollection.instance = cardCollection
         }
         else if let round = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? Round {
-            self.round = round
-            if self.round.currentTeam == self.player.getTeam() {
+            Round.instance = round
+            if Round.instance.currentTeam == Player.instance.getTeam() {
                 if !playerRoundStarted {
-                    audioToolboxManager.vibrate()
+                    AudioToolboxManager.instance.vibrate()
                     playerRoundStarted = true
                 }
             } else {
@@ -193,7 +186,7 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     func newPeerAddedToSession(peerID: MCPeerID) {}
     
     func peerDisconnectedFromSession(peerID: MCPeerID) {
-        if let peer = self.connectedPeers?[peerID], player = self.room.getPlayerWithUUID(peer) where player.getTeam() == self.player.team {
+        if let peer = self.connectedPeers?[peerID], player = Room.instance.getPlayerWithUUID(peer) where player.getTeam() == Player.instance.team {
             let alertController = UIAlertController(title: "Oops", message: self.playerDisconnectedString, preferredStyle: .Alert)
             let confirmAction = UIAlertAction(title: "OK", style: .Default, handler: { (action: UIAlertAction) in })
             alertController.addAction(confirmAction)
@@ -212,16 +205,16 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(self.reuseIdentifier, forIndexPath: indexPath) as! GameRoomViewCell
-        let cardAtIndex = cardCollection.getCards()[indexPath.row]
+        let cardAtIndex = CardCollection.instance.getCards()[indexPath.row]
         
         cell.wordLabel.text = cardAtIndex.getWord()
         
         cell.contentView.backgroundColor = UIColor.clearColor()
         
-        if self.player.isClueGiver() {
+        if Player.instance.isClueGiver() {
             cell.contentView.backgroundColor = UIColor.colorForTeam(cardAtIndex.getTeam())
             var attributedString: NSMutableAttributedString =  NSMutableAttributedString(string: cardAtIndex.getWord())
-            if cardAtIndex.getTeam() == self.player.getTeam() {
+            if cardAtIndex.getTeam() == Player.instance.getTeam() {
                 attributedString = NSMutableAttributedString(string: cardAtIndex.getWord(), attributes: [NSFontAttributeName : UIFont(name: "HelveticaNeue-Light", size: 20)!])
             }
             if cardAtIndex.isSelected() {
@@ -239,15 +232,15 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     }
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        if self.player.isClueGiver() || self.round.currentTeam != self.player.getTeam() {
+        if Player.instance.isClueGiver() || Round.instance.currentTeam != Player.instance.getTeam() {
             return
         }
         
-        let cardAtIndex = self.cardCollection.getCards()[indexPath.row]
-        self.cardCollection.getCards()[indexPath.row].setSelected()
+        let cardAtIndex = CardCollection.instance.getCards()[indexPath.row]
+        CardCollection.instance.getCards()[indexPath.row].setSelected()
         
-        if cardAtIndex.getTeam() != self.player.getTeam() {
-            if cardAtIndex.getTeam() == Team.Neutral || cardAtIndex.getTeam() == Team(rawValue: self.player.getTeam().rawValue ^ 1) {
+        if cardAtIndex.getTeam() != Player.instance.getTeam() {
+            if cardAtIndex.getTeam() == Team.Neutral || cardAtIndex.getTeam() == Team(rawValue: Player.instance.getTeam().rawValue ^ 1) {
                 self.didEndRound()
             }
         }
@@ -266,7 +259,7 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     
     // MARK: UITextFieldDelegate
     func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
-        if self.player.isClueGiver() && self.round.currentTeam == self.player.getTeam() {
+        if Player.instance.isClueGiver() && Round.instance.currentTeam == Player.instance.getTeam() {
             return true
         } else {
             return false
@@ -275,10 +268,10 @@ class GameRoomViewController: UIViewController, UICollectionViewDelegateFlowLayo
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         if textField == self.clueTextField && textField.text?.characters.count >= 1 {
-            self.round.clue = textField.text
+            Round.instance.clue = textField.text
             self.numberOfWordsTextField.becomeFirstResponder()
         } else if textField == self.numberOfWordsTextField {
-            self.round.numberOfWords = textField.text
+            Round.instance.numberOfWords = textField.text
             self.numberOfWordsTextField.resignFirstResponder()
             self.didConfirm()
         }
