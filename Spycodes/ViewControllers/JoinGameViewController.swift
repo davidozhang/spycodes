1a38e321c77aac560a42deb6d7753dc23de0480f
David Zhang <davzee@hotmail.com>
2016-07-23 01:21:56 -0700

    Refactor Create and Join Game View Controllers

--- Spycodes/ViewControllers/JoinGameViewController.swift
+++ Spycodes/ViewControllers/JoinGameViewController.swift
@@ -1,15 +1,13 @@
 import UIKit
 
 class JoinGameViewController: UIViewController, UITextFieldDelegate {
-    
-    private let player = Player.instance
-    
     @IBOutlet weak var userNameTextField: SpycodesTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        userNameTextField.delegate = self
-        userNameTextField.becomeFirstResponder()
+        
+        self.userNameTextField.delegate = self
+        self.userNameTextField.becomeFirstResponder()
     }
     
     override func didReceiveMemoryWarning() {
@@ -17,19 +15,19 @@ class JoinGameViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-        guard let text = userNameTextField.text else { return true }
+        guard let text = self.userNameTextField.text else { return true }
         
         let length = text.characters.count + string.characters.count - range.length
         return length <= 8
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        if let name = userNameTextField.text where name.characters.count >= 1 {
-            player.setPlayerName(name)
-            if (player.isHost()) {
-                performSegueWithIdentifier("pregame-room", sender: self)
+        if let name = self.userNameTextField.text where name.characters.count >= 1 {
+            Player.instance.setPlayerName(name)
+            if (Player.instance.isHost()) {
+                self.performSegueWithIdentifier("pregame-room", sender: self)
             } else {
-                performSegueWithIdentifier("lobby-room", sender: self)
+                self.performSegueWithIdentifier("lobby-room", sender: self)
             }
             return true
         }
