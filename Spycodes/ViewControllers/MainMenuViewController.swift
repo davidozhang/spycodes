5fc50f93a7877487fe35b81beb73241506b88f8b
David Zhang <davzee@hotmail.com>
2016-07-28 22:12:57 -0700

    Improve main menu appearance on iPhone 4/4S

--- Spycodes/ViewControllers/MainMenuViewController.swift
+++ Spycodes/ViewControllers/MainMenuViewController.swift
@@ -1,9 +1,19 @@
 import UIKit
 
 class MainMenuViewController: UIViewController {
+    @IBOutlet weak var spycodesLabelHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var spycodesIconTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var aboutButtonBottomConstraint: NSLayoutConstraint!
+    
     // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if UIScreen.mainScreen().bounds.size.height == 480 {
+            self.spycodesLabelHeightConstraint.constant = 0
+            self.spycodesIconTopConstraint.constant = 40
+            self.aboutButtonBottomConstraint.constant = 40
+        }
     }
 
     override func didReceiveMemoryWarning() {
