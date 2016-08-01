08c36acc1966eef2ee62f97cc71e0a96a07fe95b
David Zhang <davzee@hotmail.com>
2016-07-31 22:34:35 -0700

    Change start game requirements text

--- Spycodes/Extensions/SpycodesMessage.swift
+++ Spycodes/Extensions/SpycodesMessage.swift
@@ -7,7 +7,7 @@ class SpycodesMessage {
     
     static let hostDisconnectedString = "Host player has been disconnected."
     static let removedFromRoomString = "You have been removed from the room."
-    static let cannotStartGameString = "Check the following:\n1. Tap on a player's name to select as clue giver. There must be 1 clue giver on each team.\n2. 4 or more players are required with at least 2 players on each team."
+    static let cannotStartGameString = "Check the following:\n1. Tap on a player's name to select as clue giver. There must be 1 clue giver on each team.\n2. For mini game, 2-3 players are required with 1 cluegiver. For regular game, 4 or more players are required with at least 2 players on each team."
     
     static let playerDisconnectedString = "A player in the game has disconnected."
 }
