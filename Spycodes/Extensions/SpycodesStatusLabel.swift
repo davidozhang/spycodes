0e9008c94bbe7b5afd66a5c30711257bbf0861f7
David Zhang <davzee@hotmail.com>
2016-12-25 21:56:33 +0100

    Allow sharing in the main menu

--- Spycodes/Extensions/SpycodesStatusLabel.swift
+++ Spycodes/Extensions/SpycodesStatusLabel.swift
@@ -3,6 +3,6 @@ import UIKit
 class SpycodesStatusLabel: UILabel {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.font = UIFont(name: "HelveticaNeue-UltraLight", size: 14)
+        self.font = UIFont(name: "HelveticaNeue-Bold", size: 14)
     }
 }
