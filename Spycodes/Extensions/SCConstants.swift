bb99304e7176621df4dd8010d3cfd955f7ae9978
David Zhang <davzee@hotmail.com>
2017-03-21 01:00:31 -0700

    Use US App Store web URL

--- Spycodes/Extensions/SCConstants.swift
+++ Spycodes/Extensions/SCConstants.swift
@@ -4,7 +4,7 @@ class SCConstants {
     static let versionURL = "https://itunes.apple.com/lookup?bundleId=\(bundleID)"
     static let supportURL = "https://www.spycodes.net/support/"
     static let appStoreURL = "itms-apps://itunes.apple.com/app/id\(appID)"
-    static let appStoreWebURL = "https://itunes.apple.com/ca/app/spycodes/id1141711201?mt=8"
+    static let appStoreWebURL = "https://itunes.apple.com/app/spycodes/id1141711201?mt=8"
     static let githubURL = "https://github.com/davidozhang/spycodes"
     static let icons8URL = "https://icons8.com"
     static let websiteURL = "https://www.spycodes.net/"
