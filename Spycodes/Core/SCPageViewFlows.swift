c83c96f009ec177cb6258d60b4fb1ed61e54aa08
David Zhang <davzee@hotmail.com>
2017-12-21 01:59:11 -0800

    Set up pregame onboarding flow

--- Spycodes/Core/SCPageViewFlows.swift
+++ Spycodes/Core/SCPageViewFlows.swift
@@ -8,20 +8,48 @@ class SCPageViewFlows {
         .Pregame: [
             0:
                 SCPageViewFlowEntry([
-                   SCConstants.onboardingFlowEntryKey.displayImageName.rawValue: "Spy",
-                   SCConstants.onboardingFlowEntryKey.displayImageWidth.rawValue: 128,
-                   SCConstants.onboardingFlowEntryKey.displayImageHeight.rawValue: 128,
-                   SCConstants.onboardingFlowEntryKey.displayText.rawValue: "First onboarding view that has a lot of text, like a lot of text.. would this overflow? test test test test",
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "Welcome to Spycodes! Let's help you get started.",
                 ]),
             1:
                 SCPageViewFlowEntry([
-                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "Second onboarding view",
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "There are two types of games you could play: regular and minigame.",
                 ]),
             2:
                 SCPageViewFlowEntry([
-                    SCConstants.onboardingFlowEntryKey.displayImageName.rawValue: "Shuffle",
-                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "Third onboarding view",
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "In a regular game, you can pick a team to be on. Each team should have at least 2 players.",
                 ]),
+            3:
+                SCOnboardingFlowEntry([
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "In a minigame, you all play on Team Red against the CPU on Team Blue. Your team should have 2-3 players.",
+                    ]),
+            4:
+                SCOnboardingFlowEntry([
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "Tap a teammate to nominate as leader. The leader will be providing clues to your team for the next game.",
+                    ]),
+            5:
+                SCOnboardingFlowEntry([
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "Tap the shuffle button to randomly assign your team's leader. Tap the change button to change your team assignment.",
+                    ]),
+            6:
+                SCOnboardingFlowEntry([
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "Tap Ready when you are set. The game starts when everyone is ready.",
+                    ]),
+            7:
+                SCOnboardingFlowEntry([
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "Chevrons in the app can be tapped on. They also indicate swipe support in the direction it is pointing.",
+                    ]),
+            8:
+                SCOnboardingFlowEntry([
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "Swipe up in the pregame view to access game and category settings.",
+                    ]),
+            9:
+                SCOnboardingFlowEntry([
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "Remember, you can always access this help view by tapping on the help button.",
+                    ]),
+            10:
+                SCOnboardingFlowEntry([
+                    SCConstants.onboardingFlowEntryKey.displayText.rawValue: "You are all set for now. Swipe down to dismiss this view!",
+                    ]),
         ],
         .Game: [:]
     ]
