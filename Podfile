84e7229d307167f9157f5925de8e6d9b155b83db
David Zhang <davzee@hotmail.com>
2018-01-12 18:25:45 -0500

    Revert "Working touch visualization with mirroring"
    This reverts commit 7bf18e4f381e457ce9f72ea3f8634e8057be8c2f.

--- Podfile
+++ Podfile
@@ -2,5 +2,4 @@ use_frameworks!
 target 'Spycodes' do
 	pod 'ISEmojiView'
 	pod 'SwiftGifOrigin', '~> 1.6.1'
-	pod 'Fingertips'
 end
--- Spycodes/SCAppDelegate.swift
+++ Spycodes/SCAppDelegate.swift
@@ -1,9 +1,8 @@
 import UIKit
-import Fingertips
 
 @UIApplicationMain
 class SCAppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow? = MBFingerTipWindow(frame: UIScreen.main.bounds)
+    var window: UIWindow?
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
